b9d8e5bc9aeaefdaf712f66b5980f527c656a5c4
Chiru <cjheuristic@gmail.com>
2026-08-18 17:50:23 +0530

    Day1 (18-08-26) python

---
+++ day1.py
@@ -0,0 +1,20 @@
+""" Create a Python program that:
+
+asks the user for their name
+asks for their age
+calculates what their age will be next year
+prints a sentence containing their name and next year's age """
+
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+print(f'Hi {user_name}. Your age for the next year will be {user_age + 1}.')
+
+
+
+"""
+Day 1: Learned Python basics, expressions, values, and data types.
+Learned arithmetic operators and operator precedence.
+Learned variables, names, objects, assignment, and reassignment.
+Learned input(), type conversion, print(), f-strings, and TypeError.
+Built a simple interactive program using these concepts.
+"""
