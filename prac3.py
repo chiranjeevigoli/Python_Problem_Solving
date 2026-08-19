5c7ae281a1d365ce8b64143e29810077cab902ee
Chiru <cjheuristic@gmail.com>
2026-08-19 12:12:24 +0530

    if-else condition

---
+++ prac3.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+if number == 0:
+    print("The number is neither positive nor negative number.")
+
+elif number > 0:
+    print("The number is positive number.")
+
+else:
+    print("The number is negative number.")
